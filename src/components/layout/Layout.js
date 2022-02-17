9ec958b9b8ddfdec84542ec968d366dea2982a9f
Lucas Pontes <lucasde.apontes@gmail.com>
2022-02-17 16:42:42 -0300

    finished

---
+++ src/components/layout/Layout.js
@@ -0,0 +1,15 @@
+import { Fragment } from 'react';
+import classes from './Layout.module.css';
+import MainNavigation from './MainNavigation';
+import React from 'react';
+
+const Layout = (props) => {
+    return(
+        <Fragment>
+            <MainNavigation />
+            <main className={classes.main}>{props.children}</main>
+        </Fragment>
+    );
+};
+
+export default Layout;
